0c4cc51ae0dcc91bdda9cd7658d0957e4f3fb778
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-07-14 13:38:05 -0700

    Fix timing issues
    putting in 1000ms now actually gives you (approximately) 1 second of animation, instead of 1.5

--- AnimateForms/Animate/Animate.cs
+++ AnimateForms/Animate/Animate.cs
@@ -1,6 +1,7 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.Threading.Tasks;
-using System.Timers;
 using System.Windows.Forms;
 
 namespace AnimateForms.Animate
@@ -8,49 +9,45 @@ namespace AnimateForms.Animate
     public class Animate
     {
         private delegate int Function(float t, float b, float c, float d);
-        private static System.Timers.Timer timer;
 
-        public Animate()
+        private Function GetFunction(string function)
         {
-            timer = new System.Timers.Timer(10);
-            timer.Elapsed += Update;
-            timer.AutoReset = true;
-            timer.Enabled = true;
+            switch (function)
+            {
+                case "Linear": return Easings.Linear;
+                case "QuadIn": return Easings.QuadIn;
+                case "QuadOut": return Easings.QuadOut;
+                case "QuadInOut": return Easings.QuadInOut;
+                case "CubicIn": return Easings.CubicIn;
+                case "CubicOut": return Easings.CubicOut;
+                case "CubicInOut": return Easings.CubicInOut;
+                default: return Easings.Linear;
+            }
         }
 
-        public async Task<bool> AnimateElement(Control control, Size sizeTo, int duration, string easing)
+        public async Task<bool> AnimateResize(Control control, Size sizeTo, int duration, string easing)
         {
             int height = control.Height;
             int width = control.Width;
 
-            int heightDif = sizeTo.Height - control.Height;
-            int widthDif = sizeTo.Width - control.Width;
+            int heightDif = sizeTo.Height - height;
+            int widthDif = sizeTo.Width - width;
 
-            Function f;
-            switch (easing)
-            {
-                case "Linear": f = Easings.Linear; break;
-                case "QuadIn": f = Easings.QuadIn; break;
-                case "QuadOut": f = Easings.QuadOut; break;
-                case "QuadInOut": f = Easings.QuadInOut; break;
-                case "CubicIn": f = Easings.CubicIn; break;
-                case "CubicOut": f = Easings.CubicOut; break;
-                case "CubicInOut": f = Easings.CubicInOut; break;
-                default: f = Easings.Linear; break;
-            }
+            Function f = GetFunction(easing);
 
-            for (int i = 0; i <= duration; i++)
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            while (stopwatch.ElapsedMilliseconds < duration)
             {
-                control.Size = new Size(f(i, width, widthDif, duration),
-                                        f(i, height, heightDif, duration));
                 await Task.Delay(1);
+                int time = (int)stopwatch.ElapsedMilliseconds;
+                if (time > duration) time = duration;
+                control.Size = new Size(f(time, width, widthDif, duration),
+                                        f(time, height, heightDif, duration));
             }
 
             return true;
-        } 
-
-        private void Update(object source, ElapsedEventArgs e)
-        {
         }
     }
 }
--- AnimateForms/Form1.cs
+++ AnimateForms/Form1.cs
@@ -6,11 +6,11 @@ namespace AnimateForms
 {
     public partial class Form1 : Form
     {
-        private int d = 1000;
+        private readonly int d = 5000;
         private Size eS = new Size(300, 20);
         private Size dS = new Size(20, 20);
 
-        private Animate.Animate a = new Animate.Animate();
+        private readonly Animate.Animate a = new Animate.Animate();
 
         public Form1()
         {
@@ -19,51 +19,51 @@ namespace AnimateForms
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            a.AnimateElement(panel1, eS, d, "Linear");
-            a.AnimateElement(panel2, eS, d, "QuadIn");
-            a.AnimateElement(panel3, eS, d, "QuadOut");
-            a.AnimateElement(panel4, eS, d, "QuadInOut");
-            a.AnimateElement(panel5, eS, d, "CubicIn");
-            a.AnimateElement(panel6, eS, d, "CubicOut");
-            a.AnimateElement(panel7, eS, d, "CubicInOut");
+            a.AnimateResize(panel1, eS, d, "Linear");
+            a.AnimateResize(panel2, eS, d, "QuadIn");
+            a.AnimateResize(panel3, eS, d, "QuadOut");
+            a.AnimateResize(panel4, eS, d, "QuadInOut");
+            a.AnimateResize(panel5, eS, d, "CubicIn");
+            a.AnimateResize(panel6, eS, d, "CubicOut");
+            a.AnimateResize(panel7, eS, d, "CubicInOut");
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            a.AnimateElement(panel1, dS, d, "Linear");
-            a.AnimateElement(panel2, dS, d, "QuadIn");
-            a.AnimateElement(panel3, dS, d, "QuadOut");
-            a.AnimateElement(panel4, dS, d, "QuadInOut");
-            a.AnimateElement(panel5, dS, d, "CubicIn");
-            a.AnimateElement(panel6, dS, d, "CubicOut");
-            a.AnimateElement(panel7, dS, d, "CubicInOut");
+            a.AnimateResize(panel1, dS, d, "Linear");
+            a.AnimateResize(panel2, dS, d, "QuadIn");
+            a.AnimateResize(panel3, dS, d, "QuadOut");
+            a.AnimateResize(panel4, dS, d, "QuadInOut");
+            a.AnimateResize(panel5, dS, d, "CubicIn");
+            a.AnimateResize(panel6, dS, d, "CubicOut");
+            a.AnimateResize(panel7, dS, d, "CubicInOut");
         }
 
         private async void Button3_Click(object sender, EventArgs e)
         {
             while(true)
             {
-                a.AnimateElement(panel1, eS, d, "Linear");
-                a.AnimateElement(panel2, eS, d, "QuadIn");
-                a.AnimateElement(panel3, eS, d, "QuadOut");
-                a.AnimateElement(panel4, eS, d, "QuadInOut");
-                a.AnimateElement(panel5, eS, d, "CubicIn");
-                a.AnimateElement(panel6, eS, d, "CubicOut");
-                a.AnimateElement(panel7, eS, d, "CubicInOut");
+                a.AnimateResize(panel1, eS, d, "Linear");
+                a.AnimateResize(panel2, eS, d, "QuadIn");
+                a.AnimateResize(panel3, eS, d, "QuadOut");
+                a.AnimateResize(panel4, eS, d, "QuadInOut");
+                a.AnimateResize(panel5, eS, d, "CubicIn");
+                a.AnimateResize(panel6, eS, d, "CubicOut");
+                await a.AnimateResize(panel7, eS, d, "CubicInOut");
 
-                a.AnimateElement(panel1, dS, d, "Linear");
-                a.AnimateElement(panel2, dS, d, "QuadIn");
-                a.AnimateElement(panel3, dS, d, "QuadOut");
-                a.AnimateElement(panel4, dS, d, "QuadInOut");
-                a.AnimateElement(panel5, dS, d, "CubicIn");
-                a.AnimateElement(panel6, dS, d, "CubicOut");
-                a.AnimateElement(panel7, dS, d, "CubicInOut");
+                a.AnimateResize(panel1, dS, d, "Linear");
+                a.AnimateResize(panel2, dS, d, "QuadIn");
+                a.AnimateResize(panel3, dS, d, "QuadOut");
+                a.AnimateResize(panel4, dS, d, "QuadInOut");
+                a.AnimateResize(panel5, dS, d, "CubicIn");
+                a.AnimateResize(panel6, dS, d, "CubicOut");
+                await a.AnimateResize(panel7, dS, d, "CubicInOut");
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Button1_Click(sender, e);
+            a.AnimateResize(panel1, eS, d, "Linear");
         }
     }
 }
