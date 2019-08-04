91abf626d8558a92aa8e65019ef93a58f434447d
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-08-03 17:32:03 -0700

    Update nuget demo
    Updated demo to include all functions

--- NuGetDemo/Demo.Designer.cs
+++ NuGetDemo/Demo.Designer.cs
@@ -34,21 +34,30 @@
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button2 = new System.Windows.Forms.Button();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.panel1.Location = new System.Drawing.Point(19, 141);
+            this.panel1.Location = new System.Drawing.Point(21, 296);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(30, 30);
             this.panel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(18, 108);
+            this.button1.Location = new System.Drawing.Point(20, 263);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.Size = new System.Drawing.Size(150, 27);
             this.button1.TabIndex = 1;
             this.button1.Text = "Resize Big";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,11 +67,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(15, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(320, 36);
+            this.label1.Size = new System.Drawing.Size(486, 36);
             this.label1.TabIndex = 2;
-            this.label1.Text = "NuGet Demo Application";
+            this.label1.Text = "NuGet Demo Application v1.1.0-alpha";
             // 
             // linkLabel1
             // 
@@ -70,7 +79,7 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.Color.CadetBlue;
-            this.linkLabel1.Location = new System.Drawing.Point(15, 56);
+            this.linkLabel1.Location = new System.Drawing.Point(17, 56);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(166, 23);
             this.linkLabel1.TabIndex = 3;
@@ -81,9 +90,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(124, 108);
+            this.button2.Location = new System.Drawing.Point(176, 263);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 27);
+            this.button2.Size = new System.Drawing.Size(150, 27);
             this.button2.TabIndex = 4;
             this.button2.Text = "Resize Small";
             this.button2.UseVisualStyleBackColor = true;
@@ -104,11 +113,104 @@
             this.linkLabel2.VisitedLinkColor = System.Drawing.Color.CadetBlue;
             this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel2_LinkClicked);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 27);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Move Right";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 100);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 27);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Move Left";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel2.Location = new System.Drawing.Point(21, 133);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(30, 30);
+            this.panel2.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(176, 342);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 27);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Recolor B";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_ClickAsync);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(20, 342);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 27);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Recolor A";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel3.Location = new System.Drawing.Point(21, 375);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(30, 30);
+            this.panel3.TabIndex = 9;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(176, 180);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 27);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "MoveRelative Right";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(21, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 27);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "MoveRelative Left";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel4.Location = new System.Drawing.Point(22, 213);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(30, 30);
+            this.panel4.TabIndex = 12;
+            // 
             // Demo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 433);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.linkLabel1);
@@ -130,6 +232,15 @@
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
--- NuGetDemo/Demo.cs
+++ NuGetDemo/Demo.cs
@@ -26,12 +26,42 @@ namespace NuGetDemo
 
         private async void Button1_Click(object sender, EventArgs e)
         {
-            await a.Resize(panel1, new Size(300, 30), 1000, Easings.Linear);
+            await a.Resize(panel1, Easings.Linear, 1000, new Size(300, 30));
         }
 
         private async void Button2_Click(object sender, EventArgs e)
         {
-            await a.Resize(panel1, new Size(30, 30), 1000, Easings.Linear);
+            await a.Resize(panel1, Easings.Linear, 1000, new Size(30, 30));
+        }
+
+        private async void Button4_Click(object sender, EventArgs e)
+        {
+            await a.Move(panel2, Easings.Linear, 1000, new Point(20, panel2.Location.Y));
+        }
+
+        private async void Button3_Click(object sender, EventArgs e)
+        {
+            await a.Move(panel2, Easings.Linear, 1000, new Point(290, panel2.Location.Y));
+        }
+
+        private async void Button6_Click(object sender, EventArgs e)
+        {
+            await a.Recolor(panel3, Easings.Linear, 1000, Color.White);
+        }
+
+        private async void Button5_ClickAsync(object sender, EventArgs e)
+        {
+            await a.Recolor(panel3, Easings.Linear, 1000, Color.FromArgb(255, 128, 0));
+        }
+
+        private async void Button8_Click(object sender, EventArgs e)
+        {
+            await a.MoveRelative(panel4, Easings.Linear, 5000, new Point(-200, 0));
+        }
+
+        private async void Button7_Click(object sender, EventArgs e)
+        {
+            await a.MoveRelative(panel4, Easings.Linear, 5000, new Point(200, 0));
         }
     }
 }
